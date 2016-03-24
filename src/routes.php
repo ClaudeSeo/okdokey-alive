8f61497328b15bfc3975f368fa1c9d155dfab67b
seodongmyeong <ehdaudtj@gmail.com>
2016-03-25 04:50:19 +0900

    Update authorization

--- src/routes.php
+++ src/routes.php
@@ -2,39 +2,23 @@
 
 require 'procfs.php';
 
-$app->post('/v1/auth/login', function ($req, $res, $args) {
-    $parsedBody = $req->getParsedBody();
-    $addr = $req->getHeader('REMOTE_ADDR') || $req->getHeader('HTTP_X_FORWARDED_FOR');
-    $agent = $req->getHeader('User-Agent');
-    $this->logger->info("Authorization '/auth' " . json_encode($parsedBody) . " " . $addr . " " . json_encode($agent));
-    if ($parsedBody['key'] == $this->security) {
-        $_SESSION['authorization'] = time() * 1000;
-        return json_encode(array('status' => 'success'));
-    } else {
-        return $res->withStatus(401)->write(json_encode(array('status' => 'error')));
-    }
-});
-
-$app->delete('/v1/auth/logout', function ($req, $res, $args) {
-    session_destroy();
-    return json_encode(array('status' => 'success'));
-});
-
 $app->get('/v1/proc', function ($req, $res, $args) {
     $parsedBody = $req->getParsedBody();
     $addr = $req->getHeader('REMOTE_ADDR') || $req->getHeader('HTTP_X_FORWARDED_FOR');
     $agent = $req->getHeader('User-Agent');
-    $this->logger->info("Authorization '/auth' " . json_encode($parsedBody) . " " . $addr . " " . json_encode($agent));
-    if ($_SESSION['authorization'] && $_SESSION['authorization'] / 1000 > time() - 3600) {
-        $proc = new procfs();
-        $arr = array(
-            'cpu' => $proc->getCpuProfile()['usage'],
-            'uptime' => $proc->getUptime(),
-            'load' => $proc->getLoadAve(),
-            'disk' => $proc->getDiskSize(),
-            'memory' => $proc->getMemoryProfile()['usage']
-        );
-        return json_encode($arr);
+    $key = $req->getHeader('X-alive-key');
+    if (!is_null($key) && strcmp($key[0], $this->security) != 0) {
+        return $res->withStatus(403)->write('PERMISSION DENIED');
     }
-    return $res->withStatus(403)->write('PERMISSION DENIED');
+    $this->logger->info("Procfile System '/proc' " . json_encode($parsedBody) . " " . $addr . " " . json_encode($agent));
+    $proc = new procfs();
+    $arr = array(
+        'cpu' => $proc->getCpuProfile()['usage'],
+        'uptime' => $proc->getUptime(),
+        'load' => $proc->getLoadAve(),
+        'disk' => $proc->getDiskSize(),
+        'memory' => $proc->getMemoryProfile()['usage']
+    );
+    return json_encode($arr);
+
 });
